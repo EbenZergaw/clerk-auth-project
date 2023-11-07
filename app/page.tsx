3b44fd3026a1e3ca1e06368a7e91bc685dbfb0bc
Ebenezer Zergabachew <eben.zergaw@gmail.com>
2023-11-06 20:51:17 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div>
-      <h1 className="text-primary text-3xl">Clerk Auth Project</h1>
-      <p className="mb-5">
+      <h1 className="text-primary text-3xl text-purple-500">Clerk Auth Project</h1>
+      <p className="mb-5 text-purple-500">
         Clerk Authentication is niceeee
       </p>
     </div>
